f561ec719a3634406bc2128c6e70ff22e9582924
smokytheangel0 <34082081+smokytheangel0@users.noreply.github.com>
2019-06-20 12:18:35 -0700

    forgot a not lol

--- remote_test.sh
+++ remote_test.sh
@@ -7,7 +7,7 @@ project=$(basename $project)
 inBOX_d=$(ls target/armv7-unknown-linux-gnueabihf/debug/deps/$project-*.d)
 inBOX_d=$(basename $inBOX_d)
 inBOX=${inBOX_d:0:(-2)}
-ssh raspberry_pi "cd /home/pi; if [ -d '$project' ]; then /home/pi/.cargo/bin/cargo new $project; fi;"
+ssh raspberry_pi "cd /home/pi; if [ ! -d '$project' ]; then /home/pi/.cargo/bin/cargo new $project; fi;"
 ssh raspberry_pi "cd /home/pi/$project; /home/pi/.cargo/bin/cargo clean; /home/pi/.cargo/bin/cargo test --no-run"
 outBOX_d=$(ssh raspberry_pi "ls /home/pi/$project/target/debug/deps/$project-*.d")
 outBOX_d=$(basename $outBOX_d)
